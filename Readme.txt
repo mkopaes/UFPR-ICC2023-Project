a20b2e078b38a2fba8d3667ada54e19ac8fde5c6
Murilo <murilo.paes191@gmail.com>
2023-10-02 03:10:17 -0300

    Atualização

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+IMPLEMENTAR: Interval createInterval(x)
+
+IMPLEMENTAR: Interval powInterval(x, exp)
+
+CONFERIR   : Conferir funções multInterval e sumInterval
+
+CONFERIR   : No arquivo fonte minimumSquare.c precisamos implementar a função solveLinearSystem,
+             que nada mais é do que aplicar Eliminação de Gauss
--- src/main.c
+++ src/main.c
@@ -1,10 +1,23 @@
-#include "../include/utils.h"
+#include "../include/curveFit.h"
 #include <stdio.h>
 
 int main(){
-    Polynomial *answer = createPolynomial();
-    Table *tab = readTable();
-    
-    freePolynomial(answer);
+    int degree_N, numCoef;
+
+    scanf("%d", &degree_N);
+
+    // Se o grau é N, temos N + 1 coeficientes
+    numCoef = degree_N + 1;
+
+    Table *tab = createTable();
+    Interval *answer = minimumSquare(numCoef, tab);
+    Interval *residual = calculateResidual(answer, tab);
+
+    printVectorInterval(answer, numCoef);
+    printVectorInterval(residual, numCoef);
+
+    freeInterval(answer);
+    freeInterval(residual);
+    freeTable(tab);
     return 0;
 }
