222d6b2dfeb46ca4a9b0ccb4678f6a7228073fa3
Eric <ericvkivel1@gmail.com>
2023-10-08 12:38:40 -0300

    feat: run.sh creates resultado.out file

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+METRICA="FLOPS_DP ENERGY" 
+
 make
-likwid-perfctr -C 0-3 -g "FLOPS_DP,FLOPS_AVX,ENERGY" -m ./ajustePol < pontos.in > resultado.out
+
+for k in $METRICA
+do
+  likwid-perfctr -C 3 -g ${k} -m ./ajustePol < pontos.in > ${k}_resultado.log
+done
+
+printf "Resultado: \n" > resultado.out
+./ajustePol < pontos.in >> resultado.out
+printf "\n" >> resultado.out
+
+echo "Build Linear System" >> resultado.out
+cat FLOPS_DP_resultado.log | grep -m 1 "DP MFLOP/s (scalar assumed)" >> resultado.out
+cat FLOPS_DP_resultado.log | grep -m 1 "DP MFLOP/s (AVX assumed)" >> resultado.out
+cat ENERGY_resultado.log | grep -m 1 "Energy Core" >> resultado.out
+printf "\n" >> resultado.out
+echo "Solve Linear System" >> resultado.out
+cat FLOPS_DP_resultado.log | grep -m 2 "DP MFLOP/s (scalar assumed)" | tail -n 1 >> resultado.out
+cat FLOPS_DP_resultado.log | grep -m 2 "DP MFLOP/s (AVX assumed)" | tail -n 1 >> resultado.out
+cat ENERGY_resultado.log | grep -m 2 "Energy Core" | tail -n 1 >> resultado.out
+
 make purge
