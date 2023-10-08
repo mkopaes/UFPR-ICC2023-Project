f4a3e89b4f8e8a7dd4ce29df0230a3a111d1c0b6
Murilo <murilo.paes191@gmail.com>
2023-10-08 18:50:38 -0300

    Update table.h

--- include/table.h
+++ include/table.h
@@ -4,6 +4,8 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+// Estrutura para armazenar uma tabela com "numPoints" pares ordenados (x, y)
+// Os dados da tabela são armazenados num vetor de intervalos "x" e "y"
 typedef struct {
   int numPoints;
   Interval *x;
@@ -11,8 +13,8 @@ typedef struct {
 } Table;
 
 // Lê do stdin um valor K e logo em seguida lê K dados no modelo "x y".
-// Cria uma tabela intervalar com os dados lidos. Retorna um ponteiro para a
-// tabela criada.
+// Cria uma tabela intervalar com os dados lidos. 
+// Retorna um ponteiro para a tabela criada.
 Table *createTable();
 
 // Libera a memória reservada por um tipo Table.
