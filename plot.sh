49f9a66d18463af9ea70a0489e44bb801d094110
Eric <ericvkivel1@gmail.com>
2023-11-29 00:07:12 -0300

    gnuplot test

---
+++ plot.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+if [ ! -d "graphs" ]; then
+  mkdir "graphs"
+fi
+
+OUT=("Cache_miss_L2_Gera" "Cache_miss_L2_Residuo"
+     "Banda_De_Memoria_Gera" "Banda_De_Memoria_Residuo"
+     "Tempo_Medio_Gera" "Tempo_Medio_Solve" "Tempo_Medio_Residuo"
+     "FlopsDP_Gera" "FlopsDP_Solve" "FlopsDP_Residuo"
+     "FlopsAVXDP_Gera" "FlopsAVX_Solve" "FlopsAVX_Residuo")
+
+FILENAMES=("L2Gera.csv" "L2Residuo.csv"
+           "L3Gera.csv" "L3Residuo.csv"
+           "TempoGera.csv" "TempoSolve.csv" "TempoResiduo.csv"
+           "FlopsDPGera.csv" "FlopsDPSolve.csv" "FlopsDPResiduo.csv"
+           "FlopsAVXGera.csv" "FlopsAVXSolve.csv" "FlopsAVXResiduo.csv")
+
+TITLES=("Cache miss L2 Gera" "Cache miss L2 Residuo"
+        "Banda De Memória Gera" "Banda De Memória Residuo"
+        "Tempo Médio Gera" "Tempo Médio Solve" "Tempo Médio Residuo"
+        "FlopsDP Gera" "FlopsDP Solve" "FlopsDP Residuo"
+        "FlopsAVXDP Gera" "FlopsAVX Solve" "FlopsAVX Residuo")
+
+YLABELS=("Cache Miss L2 (ratio)" "Cache Miss L2 (ratio)"
+         "Banda de Memória (MBYTE/s)" "Banda de Memória (MBYTE/s)"
+         "Tempo (milisegundos)" "Tempo (milisegundos)" "Tempo (milisegundos)"
+         "MFLOP/s" "MFLOP/s" "MFLOP/s"
+         "MFLOP/s" "MFLOP/s" "MFLOP/s")
+
+for i in {0..9}
+do
+  gnuplot <<- EOM
+  set terminal png size 1080, 1080 enhanced font 'Helvetica,20' linewidth 3
+  set output "./graphs/${OUT[$i]}.png"
+  set title "${TITLES[$i]}"
+  set xlabel "Número de pontos"
+  set ylabel "${YLABELS[$i]}"
+  set grid
+  set logscale x
+
+  set datafile separator ","
+
+  plot "./resultados/${FILENAMES[$i]}" \
+          using 1:2 \
+          w lp \
+          lt 5 \
+          lc 7 \
+          title 'Sem otimização (V1)', \
+       "./resultados/${FILENAMES[$i]}"\
+          using 1:3 \
+          w lp \
+          lt 7 \
+          lc 6 \
+          title 'Com otimização (V2)'
+EOM
+done
+
+# gnuplot <<- EOM
+# set terminal png size 1080, 1080 enhanced font 'Helvetica,20' linewidth 3 
+# set output "./graphs/FLOPS_MMM.png"
+# set title "Operações Aritméticas MMM"
+# set xlabel "Tamanho da matriz"
+# set ylabel "MFLOP/s"
+# set grid
+#
+# set datafile separator ","
+#
+# plot "./resultados/AritmeticaMatMat.csv" \
+#         using 1:2 \
+#         w lp \
+#         lt 5 \
+#         lc 7 \
+#         title 'FLOPS DP Sem otimização', \
+#      "./resultados/AritmeticaMatMat.csv"\
+#         using 1:3 \
+#         w lp \
+#         lt 7 \
+#         lc 6 \
+#         title 'FLOPS DP Com otimização', \
+#      "./resultados/AritmeticaMatMat.csv"\
+#         using 1:4 \
+#         w lp \
+#         lt 7 \
+#         lc 9 \
+#         title 'FLOPS AVX Com otimização', \
+#      "./resultados/AritmeticaMatMat.csv"\
+#         using 1:5 \
+#         w lp \
+#         lt 7 \
+#         lc 4 \
+#         title 'FLOPS AVX Com otimização'
+# EOM
+#
+# gnuplot <<- EOM
+# set terminal png size 1080, 1080 enhanced font 'Helvetica,20' linewidth 3
+# set output "./graphs/FLOPS_MVM.png"
+# set title "Operações Aritméticas MVM"
+# set xlabel "Tamanho da matriz"
+# set ylabel "MFLOP/s"
+# set grid
+#
+# set datafile separator ","
+#
+# plot "./resultados/AritmeticaMatMat.csv" \
+#         using 1:2 \
+#         w lp \
+#         lt 5 \
+#         lc 7 \
+#         title 'FLOPS DP Sem otimização', \
+#      "./resultados/AritmeticaMatVet.csv"\
+#         using 1:3 \
+#         w lp \
+#         lt 7 \
+#         lc 6 \
+#         title 'FLOPS DP Com otimização', \
+#      "./resultados/AritmeticaMatVet.csv"\
+#         using 1:4 \
+#         w lp \
+#         lt 7 \
+#         lc 9 \
+#         title 'FLOPS AVX Com otimização', \
+#      "./resultados/AritmeticaMatVet.csv"\
+#         using 1:5 \
+#         w lp \
+#         lt 7 \
+#         lc 4 \
+#         title 'FLOPS AVX Com otimização'
+# EOM
---
+++ run.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+# Eric Kivel - GRR20220069 | Murilo Paes - GRR20190158
+
+# Ns="64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000 1000000 10000000 100000000"
+Ns="64 128 200 256"
+METRICA="FLOPS_DP L3 L2CACHE" 
+LOGS_FOLDER="logs/"
+RESULT_FOLDER="resultados/"
+VERSIONS="V1 V2"
+
+# ================================ PREPARA AMBIENTE ================================
+for VERSION in $VERSIONS
+do
+  cd ${VERSION}
+  make
+  cd ..
+done
+
+if [ ! -d "${RESULT_FOLDER}" ]; then
+  mkdir "${RESULT_FOLDER}"
+fi
+
+if [ ! -d "${LOGS_FOLDER}" ]; then
+  mkdir "${LOGS_FOLDER}"
+fi
+
+# ================================ PERCORRE AS VERSOES ================================
+for n in $Ns
+do
+  echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+  printf "\n\n N = ${n}\n\n"
+
+  # ================================ PERCORRE OS PONTOS ================================
+  for VERSION in $VERSIONS
+  do
+    cd ${VERSION}
+    # ============================= PERCORRE AS METRICAS LIKWID =============================
+    for k in $METRICA
+    do
+      ../gera_entrada $n | likwid-perfctr -C 3 -g ${k} -m ./ajustePol > ../${LOGS_FOLDER}${k}_${n}_${VERSION}.log
+    done
+
+    ../gera_entrada $n | ./ajustePol > ../${LOGS_FOLDER}TIME_${n}_${VERSION}.log
+    
+    cd ..
+  done
+
+  # Tempo 
+  TIME_NAMES="TempoGera TempoSolve TempoResiduo"
+  GROUP="TIME"
+  for NAME in $TIME_NAMES
+  do
+    if [ ! -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      touch "${RESULT_FOLDER}${NAME}.csv"
+    fi
+
+    if [ -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      echo -n "$n," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V1.log | grep -A 1 "${NAME}" | tail -n 1 | tr -d '\n' >> "${RESULT_FOLDER}${NAME}.csv"
+      echo -n "," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V2.log | grep -A 1 "${NAME}" | tail -n 1 >> "${RESULT_FOLDER}${NAME}.csv"
+    fi
+  done
+
+  # Mem 
+  L3_NAMES="L3Gera L3Residuo"
+  GROUP="L3"
+  i=1
+  for NAME in $L3_NAMES
+  do
+    if [ ! -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      touch "${RESULT_FOLDER}${NAME}.csv"
+    fi
+
+    if [ -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      echo -n "$n," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V1.log | grep -m $i "L3 bandwidth" | tail -n 1 | grep -oE '[0-9]+\.[0-9]+' | tr -d '\n' >> "${RESULT_FOLDER}${NAME}.csv"
+      echo -n "," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V2.log | grep -m $i "L3 bandwidth" | tail -n 1 | grep -oE '[0-9]+\.[0-9]+'  >> "${RESULT_FOLDER}${NAME}.csv"
+      let "i+=2"
+    fi
+  done
+
+  #Cache 
+  L2CACHE_NAMES="L2Gera L2Residuo"
+  GROUP="L2CACHE"
+  i=1
+  for NAME in $L2CACHE_NAMES
+  do
+    if [ ! -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      touch "${RESULT_FOLDER}${NAME}.csv"
+    fi
+
+    if [ -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      echo -n "$n," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V1.log | grep -m $i "L2 miss ratio" | tail -n 1 | grep -oE '[0-9]+\.[0-9]+' | tr -d '\n' >> "${RESULT_FOLDER}${NAME}.csv"
+      echo -n "," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V2.log | grep -m $i "L2 miss ratio" | tail -n 1 | grep -oE '[0-9]+\.[0-9]+' >> "${RESULT_FOLDER}${NAME}.csv"
+      let "i+=2"
+    fi
+  done
+
+  # Flops 
+  FLOPS_NAMES="FlopsDPGera FlopsDPSolve FlopsDPResiduo"
+  GROUP="FLOPS_DP"
+  i=1
+  for NAME in $FLOPS_NAMES
+  do
+    if [ ! -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      touch "${RESULT_FOLDER}${NAME}.csv"
+    fi
+
+    if [ -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      echo -n "$n," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V1.log | grep -m $i "DP MFLOP\/s" | tail -n 1 | grep -oE '[0-9]+(\.[0-9]+)?' | tr -d '\n' >> "${RESULT_FOLDER}${NAME}.csv"
+      echo -n "," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V2.log | grep -m $i "DP MFLOP\/s"| tail -n 1 | grep -oE '[0-9]+(\.[0-9]+)?' >> "${RESULT_FOLDER}${NAME}.csv"
+      let "i+=2"
+    fi
+  done
+
+  # AVX 
+  FLOPS_NAMES="FlopsAVXGera FlopsAVXSolve FlopsAVXResiduo"
+  GROUP="FLOPS_DP"
+  i=1
+  for NAME in $FLOPS_NAMES
+  do
+    if [ ! -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      touch "${RESULT_FOLDER}${NAME}.csv"
+    fi
+
+    if [ -f "${RESULT_FOLDER}${NAME}.csv" ]; then
+      echo -n "$n," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V1.log | grep -m $i "AVX DP MFLOP\/s" | tail -n 1 | grep -oE '[0-9]+(\.[0-9]+)?' | tr -d '\n' >> "${RESULT_FOLDER}${NAME}.csv"
+      echo -n "," >> "${RESULT_FOLDER}${NAME}.csv" 
+      cat ${LOGS_FOLDER}${GROUP}_${n}_V2.log | grep -m $i "AVX DP MFLOP\/s"| tail -n 1 | grep -oE '[0-9]+(\.[0-9]+)?'>> "${RESULT_FOLDER}${NAME}.csv"
+      let "i+=1"
+    fi
+  done
+
+done
+
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+# make clean
